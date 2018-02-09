2c322569896ae92dfc26864fe843c194edfc1377
rahul <rahulsharmacs50@gmail.com>
2018-02-10 00:41:24 +0530

    created the home page view

--- crmapp/marketing/views.py
+++ crmapp/marketing/views.py
@@ -3,4 +3,12 @@ from __future__ import unicode_literals
 
 from django.shortcuts import render
 
+from django.views.generic.base import TemplateView
+
 # Create your views here.
+
+class HomePage(TemplateView):
+	""" Because our needs are so simple, all we have to do is assign one value; template_name.
+	The home.html file we will create as a response to this request"""
+
+	template_name = 'marketing/home.html'
