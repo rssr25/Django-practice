2234ff059610e29ca6cf9298169ec5dd321f929a
rahul <rahulsharmacs50@gmail.com>
2018-02-12 19:05:17 +0530

    created the home page URL conf

--- crmapp/urls.py
+++ crmapp/urls.py
@@ -13,9 +13,34 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import url
+from django.conf.urls import url, patterns, include
 from django.contrib import admin
+from marketing.views import HomePage
 
-urlpatterns = [
-    url(r'^admin/', admin.site.urls),
-]
+urlpatterns = patterns('',
+
+		#Marketing pages
+		url(r'^$', HomePage.as_view(), name="home"),
+
+		#Subscriber related URLs
+
+
+
+		#Admin URL
+
+
+
+		#Login/Logout URLs
+
+
+
+		#Account related URLs
+
+
+
+		#Contact related URLs
+
+
+
+		#Communication related URLs
+	)
