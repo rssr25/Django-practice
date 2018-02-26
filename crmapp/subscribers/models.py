690f023909e9fa290988bf977a0f805010de45e6
rahul <rahulsharmacs50@gmail.com>
2018-02-26 21:47:36 +0530

    created the payment processing code

--- crmapp/subscribers/models.py
+++ crmapp/subscribers/models.py
@@ -3,6 +3,9 @@ from __future__ import unicode_literals
 
 from django.db import models
 from django.contrib.auth.models import User
+from django.conf import settings
+
+import stripe
 
 # Create your models here.
 class Subscriber(models.Model):
@@ -18,3 +21,27 @@ class Subscriber(models.Model):
 
 	def __unicode__(self):
 		return u"%s's Subscription Info" % self.user_rec
+
+	def charge(self, request, email, fee):
+		stripe.api_key = settings.STRIPE_SECRET_KEY
+
+		#get the credit card details submitted by the form
+		token = request.POST['stripeToken']
+
+		#create a customer
+		stripe_customer = stripe.Customer.create(
+				card=token,
+				description=email
+			)
+
+		#sve the stripe ID to the customer's profile
+		self.stripe_id = stripe_customer.id
+		self.save()
+
+		stripe.charge.create(
+				amount=fee, #in cents
+				currency="usd",
+				customer=stripe_customer.id
+			)
+
+		return stripe_customer
