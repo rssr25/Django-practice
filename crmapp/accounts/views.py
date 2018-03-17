6b7e75008e9a0d7737875927911d70172967d1e1
rahul <rahulsharmacs50@gmail.com>
2018-03-17 19:15:26 +0530

    added search to the account list page

--- crmapp/accounts/views.py
+++ crmapp/accounts/views.py
@@ -11,13 +11,25 @@ from .models import Account
 
 class AccountList(ListView):
 	model = Account
+	paginate_by = 12
 	template_name = 'accounts/account_list.html'
 	context_object_name = 'accounts'
 
 	def get_queryset(self):
-		account_list = Account.objects.filter(owner=self.request.user)
-		return account_list
+		try:
+			a = self.request.GET.get('account',)
+		except KeyError:
+			a = None
+		if a:
+			account_list = Account.objects.filter(
+					name__icontains=a,
+					owner = self.request.user
+				)
+		else:
+			account_list = Account.objects.filter(owner=self.request.user)
 
+		return account_list
+		
 	@method_decorator(login_required)
 	def dispatch(self, *args, **kwargs):
 		return super(AccountList, self).dispatch(*args, **kwargs)
